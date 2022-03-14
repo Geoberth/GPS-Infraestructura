17e33b18cfcb5c1ce4ae200cbd25588a0b77d9ad
Norberto <53538487+Geoberth@users.noreply.github.com>
2022-03-14 12:00:35 -0600

    Sacrificio

---
+++ app/src/main/java/com/example/geosegalmex/Infra_Sacrificio/Sacrificio_1.java
@@ -0,0 +1,19 @@
+package com.example.geosegalmex.Infra_Sacrificio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.geosegalmex.R;
+
+public class Sacrificio_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sacrificio1);
+
+
+
+    }
+}
